89b536c976c1c2f5fd4b49d7c535715ef821e8af
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-19 19:09:09 +0530

    .

--- arrays/basic.js
+++ arrays/basic.js
@@ -2,3 +2,13 @@ const myARR = [1,2,3,4,5,6]
 const name = ["Ram","Shyam"]
 const arr2 = new Array(1,2,3,4)
 console.log(arr2);
+
+//Array Methods 
+
+myARR.push(6)
+myARR.push(7)
+myARR.pop()
+//console.log(myARR);
+myARR.unshift(9)
+myARR.shift()
+console.log(myARR);
