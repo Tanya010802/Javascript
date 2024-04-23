e28dad594df93e0a0eaf6b6a21b1253da03fda36
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-23 22:49:25 +0530

    .

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+// Primitive 
+// 7 types: String , Number , Boolean , Null , Undefined , symbol , Big int 
+
+
+// REFERENCE (NON PRIMITIVE)
+// arrays , objects , functions
+
+//const heros  = ['Batman', 'Superman'];
+//let heros = [];
+//heros[0] = "Batman";
+
+//let myOBJ = { 
+  //  NAME: "TANYA SINGH",
+    //AGE :21,
+//}
+//console.log(myOBJ);
+
+
+//const myfunction = function()
+//{
+  //  console.log("hello world")
+//}
+
+
+//HOW TO FIND THE DATATYPE OF ANY GIVEN VARIABLE 
+console.log(typeof "Hello World");   // returns string
+console.log(typeof 456);             // returns number
+console.log(typeof true);           // returns boolean
+console.log(typeof null);            // returns object (important)
+console.log(typeof undefined);       // returns undefined
+console.log(typeof Symbol());        // returns symbol
+console.log(BigInt(987654321987654321)); //returns bigint
