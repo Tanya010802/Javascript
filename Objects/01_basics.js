e6aabd18c88d12c00957fa4370ef00f1f07bc03f
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-27 21:43:11 +0530

    Update 01_basics.js

--- Objects/01_basics.js
+++ Objects/01_basics.js
@@ -5,10 +5,11 @@
 //Object Literals 
 const JsUser  =  {
     name:"Tanya",
+    "full name":"Tanya Singh",// this is no way to acces this with dot operator thats why second option for accessing is better
     age: 21,
     location:"Noida",
     gmail:"xxx@gmail.com",
     isloggedin:false
 }
 console.log(JsUser.gmail)// this is actually a wrong method to access the element
-
+console.log(JsUser["gmail"])// this is the correct and better way to define 
