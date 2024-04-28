b64790cc9186ee4a50806c0583289dd7227d2bd0
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-28 16:29:30 +0530

    Update 01_basics.js

--- Objects/01_basics.js
+++ Objects/01_basics.js
@@ -3,13 +3,22 @@
 //when constructor creates objects it is singleton(object.create)
 
 //Object Literals 
+
+const mysym = Symbol("key1")
 const JsUser  =  {
     name:"Tanya",
     "full name":"Tanya Singh",// this is no way to acces this with dot operator thats why second option for accessing is better
     age: 21,
+    [mysym]: "mykey1",
     location:"Noida",
     gmail:"xxx@gmail.com",
     isloggedin:false
 }
 console.log(JsUser.gmail)// this is actually a wrong method to access the element
 console.log(JsUser["gmail"])// this is the correct and better way to define 
+//console.log(JsUser.mysym)//WITH THIS DOT OPERATOR THIS IS NO PROBLEM WITH THE ACCESS BUT WHILE PRINTING Data type it will show string rather than symbol 
+//console.log(typeof JsUser.mysym)// symbol keyed property can't be accessed using dot notation
+// Thats why it is used with square brackets only 
+console.log(JsUser[mysym])
+console.log(typeof JsUser[mysym])
+console.log(JsUser)
